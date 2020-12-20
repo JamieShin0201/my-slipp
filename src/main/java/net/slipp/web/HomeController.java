7b2f683a7513eca92bbf79d56dc48906b35d734e
JamieShin0201 <jamieshin1450@gmail.com>
2020-12-20 23:07:54 +0900

    Implement question list

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,13 +1,22 @@
 package net.slipp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import net.slipp.domain.QuestionReposetory;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private QuestionReposetory questionReposetory;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("questions", questionReposetory.findAll());
+		
 		return "index";
 	}
 	
