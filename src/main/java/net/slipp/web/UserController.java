c73bd0e6c13051acd9ca52c982a035a0e2880f0c
JamieShin0201 <jamieshin1450@gmail.com>
2020-12-20 19:36:33 +0900

    Implement UpdateUser
    Added html templates
    Added Jpa, H2 dateabase

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,30 +1,58 @@
 package net.slipp.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
-	private List<User> users = new ArrayList<User>();
 	
-	@GetMapping("/list")
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		
-		return "list";
+		return "/user/list";
 	}
 	
-	@PostMapping("/create")
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@PostMapping("")
 	public String create(User user) {
-		System.out.println("user : " + user);
-		users.add(user);
+		userRepository.save(user);
 		
-		return "redirect:/list";
+		return "redirect:/users";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		model.addAttribute("user", userRepository.findById(id).get());
+		
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		
+		userRepository.save(user);
+		
+		return "redirect:/users";
 	}
 	
 }
