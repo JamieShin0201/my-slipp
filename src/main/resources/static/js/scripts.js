d340f64ed0e7906e732b04deb3acc22daaa6eeca
JamieShin0201 <jamieshin1450@gmail.com>
2020-12-21 16:43:23 +0900

    Implement answer api
    added ajax for adding, deleting answer
    added rest controller

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,73 @@
 String.prototype.format = function() {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
-    return typeof args[number] != 'undefined'
-        ? args[number]
-        : match
-        ;
-  });
+	var args = arguments;
+	return this.replace(/{(\d+)}/g, function(match, number) {
+		return typeof args[number] != 'undefined'
+			? args[number]
+			: match
+			;
+	});
 };
+
+function onSuccess(data, status) {
+	console.log(data);
+	const answerTemplate = $("#answerTemplate").html();
+	const template = answerTemplate.format(data.writer.userId, data.formattedCreateDate, data.contents, data.question.id, data.id);
+
+	$(template).insertBefore(".answer-write");
+
+	$(".answer-write textarea").val("");
+}
+
+function onError() {
+
+}
+
+function addAnswer(e) {
+	e.preventDefault();
+
+	const queryString = $(".answer-write").serialize();
+	const url = $(".answer-write").attr("action");
+
+	$.ajax({
+		type: 'post',
+		url: url,
+		data: queryString,
+		dataType: 'json',
+		error: onError,
+		success: onSuccess
+	});
+}
+
+$(".answer-write > input[type=submit]").click(addAnswer);
+
+$(document).on('click', '.link-delete-article', function(e) {
+	e.preventDefault();
+	
+	const deleteBtn = $(this);
+	const url = $(this).attr("href");
+	
+	$.ajax({
+		type: 'delete',
+		url: url,
+		dataType: 'json',
+		error: function(xhr, status) {
+			console.log('error');
+		},
+		success: function(data, status) {
+			console.log('success');
+			if(data.valid) {
+				deleteBtn.closest("article").remove();
+			} else {
+				alert(data.errorMessage);
+			}
+		}
+	});
+	
+});
+
+
+
+
+
+
+
