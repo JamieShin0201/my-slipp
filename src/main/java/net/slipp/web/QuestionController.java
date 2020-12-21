e57cb98aacccaab1f48b8541cf16373460695f3d
JamieShin0201 <jamieshin1450@gmail.com>
2020-12-21 12:20:27 +0900

    Fixed Question, UserDomain mapping
    Added User to Question domain

--- src/main/java/net/slipp/web/QuestionController.java
+++ src/main/java/net/slipp/web/QuestionController.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +19,7 @@ import net.slipp.domain.User;
 public class QuestionController {
 
 	@Autowired
-	private QuestionReposetory QuestionReposetory;
+	private QuestionReposetory questionReposetory;
 
 	@GetMapping("/form")
 	public String form(HttpSession session) {
@@ -34,11 +36,18 @@ public class QuestionController {
 		}
 
 		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
-		Question newQuestion = new Question(sessionedUser.getUserId(), title, contents);
+		Question newQuestion = new Question(sessionedUser, title, contents);
 
-		QuestionReposetory.save(newQuestion);
+		questionReposetory.save(newQuestion);
 
 		return "redirect:/";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable Long id, Model model) {
+		model.addAttribute("question", questionReposetory.findById(id).get());
+		
+		return "/qna/show";
 	}
 
 }
